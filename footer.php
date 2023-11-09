161e8f13c3288e334a15c16d5d08f9e76be807ba
fumiko-1103 <138564387+fumiko-1103@users.noreply.github.com>
2023-11-09 11:47:13 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,27 @@
+ <!--フッターフリーダイヤル-->
+ <footer>
+    <div class="bgn-call2">
+      <h2><span class="bold">B</span>&nbsp;便利屋&nbsp;<span class="bold">G</span>&nbsp;グループ&nbsp;<span
+          class="bold">N</span>&nbsp;何でもやります</h2>
+      <div class="free-cool">
+        <h2><i class="bi bi-telephone-fill"></i> to&nbsp;call&nbsp; <a href="tel:0800-100-2800">0800-100-2800
+          </a></h2>
+      </div>
+      <div class="contact-footer-copy">copyright&copy;&nbsp;Co.,Ltd.B・G・N All Rights Reserved.</div>
+    </div>
+
+  </footer>
+  <!-- フッターここまで -->
+  <?php wp_footer(); ?>
+</body>
+
+</html> 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="便利屋BGNの広告サイトです">
+  <title>便利屋B・G・N</title>
+  <!-- <link rel="stylesheet" href="css/destyle.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@500&family=Limelight&family=Montserrat:wght@400;700&family=Noto+Sans+JP&family=Open+Sans&family=Syne:wght@700&display=swap"
+    rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"> -->
+
+  <?php wp_head(); ?>
+</head>
+
+
+
+<body>
+
+ 
+
+
+
